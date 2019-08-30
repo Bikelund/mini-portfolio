4b340628fe7ad0ebd17c759d7ca500a4dfbd4569
Bikelund <nicklas_barklund@hotmail.com>
2019-08-30 10:47:45 +0200

    Chart added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Bar, Pie, HorizontalBar } from 'react-chartjs-2'
 
 const ContactPage = () => (
   <Layout>
@@ -13,16 +13,90 @@ const ContactPage = () => (
 
       <div>
         <ul class="ul-contact">
-          <li><a href="https://www.linkedin.com/in/nicklas-barklund/" target="_blank">Linkedin</a></li>
-          <li><a href="https://github.com/Bikelund" target="_blank">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/nicklas-barklund/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+          <li><a href="https://github.com/Bikelund" target="_blank" rel="noopener noreferrer">Github</a></li>
         </ul>
-        </div>
+      </div>
 
       <div class="buddy"></div>
 
     </div>
+
+    <Bar
+    data={{
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Pizzor konsumerade per månad',
+            backgroundColor: 'pink',
+            borderColor: 'green',
+            borderWidth: 1,
+            hoverBackgroundColor: 'green',
+            hoverBorderColor: 'pink',
+            data: [5, 9, 4, 1, 6, 5, 2],
+          }
+        ]
+      }}
+    width={100}
+    height={50}
+    options={{
+    maintainAspectRatio: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+    }}
+/>
+
+ <Pie data={{
+        labels: ['Pacman', 'Not Pacman'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: ['yellow', 'black'],
+            borderColor: 'black',
+            borderWidth: 1,
+            data: [80, 20],
+          }
+        ]
+      }}
+    width={100}
+    height={50}
+    options={{
+    maintainAspectRatio: true,
+    }}
+/>
     
-    
+<HorizontalBar data={{
+        labels: ['ATR Downtown', 'ATR Town', 'Cinelli Vigorelli', 'Havoc', 'Panasonic', 'Pompino', 'Trainer'],
+        datasets: [
+          {
+            label: 'Most used bikes (kilometer)',
+            backgroundColor: ['#FFE4A8', '#E8C899', '#FFD6B4', '#E8B199', '#FFB6A8', '#E89999', '#FFA8D2'],
+            borderColor: 'black',
+            borderWidth: 1,
+            hoverBackgroundColor: ['#FFE4A8', '#E8C899', '#FFD6B4', '#E8B199', '#FFB6A8', '#E89999', '#FFA8D2'],
+            hoverBorderColor: ['#FFE4A8', '#E8C899', '#FFD6B4', '#E8B199', '#FFB6A8', '#E89999', '#FFA8D2'],
+            data: [1598, 547, 890, 20, 83, 548, 371]
+          }
+        ]
+      }}
+    width={100}
+    height={50}
+    options={{
+    maintainAspectRatio: true,
+    scales: {
+      xAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+    }}
+/>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,13 +8,13 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h2>My works</h2>
     <div class="x45 works">
-      <h3><a class="works-a" href="https://bikelund.github.io/X45-001/" target="_blank">X45-001 Czar'Krang Colossus Transit</a></h3>
+      <h3><a class="works-a" href="https://bikelund.github.io/X45-001/" target="_blank" rel="noopener noreferrer">X45-001 Czar'Krang Colossus Transit</a></h3>
     </div>
     <div class="oakley works">
-      <h3><a class="works-a-b" href="https://samirergaibi.github.io/oakley/" target="_blank">Oakley</a></h3>
+      <h3><a class="works-a-b" href="https://samirergaibi.github.io/oakley/" target="_blank" rel="noopener noreferrer">Oakley</a></h3>
     </div>
     <div class="f4 works">
-      <h3><a class="works-a" href="https://tobberos.github.io/Grupp6/" target="_blank">Fantastic Four</a></h3>
+      <h3><a class="works-a" href="https://tobberos.github.io/Grupp6/" target="_blank" rel="noopener noreferrer">Fantastic Four</a></h3>
     </div>
   </Layout>
 )
